c6fef6fb53324124fe2154d86e418a527a1fd113
S13740 <tigerus72@gmail.com>
2022-12-28 10:01:36 +0900

    적정성판단결과 에러분기

--- src/pages/2_judge/JudgeResult.js
+++ src/pages/2_judge/JudgeResult.js
@@ -15,61 +15,70 @@ function JudgeResult() {
   //const resultData = judgeResultData;
   const judgeItemList = judgeData;
   const { state } = useLocation();
-  const result = state.result;
-  let answer = state.value;
+  console.log(state);
+  
   const navigate = useNavigate();
-  if(answer.length === 0 || !answer) {
+  if(!state) {
     return (
-      "에러페이지"
+      <div>에러페이지</div>
     )
   }else {
-    function cbFooter(idx, navigate, link) {
-      if(result) {
-        //적합
-        navigate(link);
-      }else {
-        //부적합
+    const result = state.result;
+    let answer = state.value;
+    if(answer.length === 0 || !answer) {
+      return (
+        <div>에러페이지</div>
+      )
+    }else {
+      function cbFooter(idx, navigate, link) {
+        if(result) {
+          //적합
+          navigate(link);
+        }else {
+          //부적합
+        }
       }
-    }
-    return (
-      <>
-        <Header pageId={4}></Header>
-        <h3><b>적합성 적정성 고객정보 확인서(개인사업자용)</b></h3>
-        <br />
-        <h5 style={{ backgroundColor: "LightCyan" }}>적합성 적정성 판단결과를 확인해 보시고 다음단계를 이어서 진행해주시기 바랍니다.</h5>
-        <Table>
-          <thead>
-          </thead>
+      return (
+        <>
+          <Header pageId={4}></Header>
+          <h3><b>적합성 적정성 고객정보 확인서(개인사업자용)</b></h3>
           <br />
-          <tbody>
-            <tr>
-              <td align="left"><b>적합성 적정성 판단 결과</b></td>
-              <td align="right">{result?"적합":"부적합"}</td>
-            </tr>
+          <h5 style={{ backgroundColor: "LightCyan" }}>적합성 적정성 판단결과를 확인해 보시고 다음단계를 이어서 진행해주시기 바랍니다.</h5>
+          <Table>
+            <thead>
+            </thead>
             <br />
-            <tr align="left">
-              <b>적성 정보</b></tr>
-            {answer.map(function (data, idx) {
-              return (
-                <tr align="left">
-                  <td>{judgeItemList[idx].title}</td>
-                  <td>{(typeof data === "object")?data.value:data}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-  
-        </Table>
-        <Footer obj={{
-          type: "button",  
-          disabled: false,
-          text: ["확인"],  
-          link: "/judgestep3",  
-          callbackId: cbFooter
-        }} ></Footer>
-      </>
-    )
+            <tbody>
+              <tr>
+                <td align="left"><b>적합성 적정성 판단 결과</b></td>
+                <td align="right">{result?"적합":"부적합"}</td>
+              </tr>
+              <br />
+              <tr align="left">
+                <b>적성 정보</b></tr>
+              {answer.map(function (data, idx) {
+                return (
+                  <tr align="left">
+                    <td>{judgeItemList[idx].title}</td>
+                    <td>{(typeof data === "object")?data.value:data}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+    
+          </Table>
+          <Footer obj={{
+            type: "button",  
+            disabled: false,
+            text: ["확인"],  
+            link: "/judgestep3",  
+            callbackId: cbFooter
+          }} ></Footer>
+        </>
+      )
+    }
   }
+  
   
 }
 export default JudgeResult;
